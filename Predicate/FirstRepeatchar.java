7cd3f8e5331aa3cd95c6a1d69be1bdb7aab88d1c
Manish  Mishra <manishweb9199@gmail.com>
2026-07-10 21:36:40 +0530

    Create FirstRepeatchar.java

---
+++ Predicate/FirstRepeatchar.java
@@ -0,0 +1,13 @@
+public class FirstRepeatChar{
+    public static void main(String[] args) {
+        String str = "programming";
+
+        Character ch = str.chars()
+                .mapToObj(c -> (char) c)
+                .filter(c -> str.indexOf(c) != str.lastIndexOf(c))
+                .findFirst()
+                .orElse(null);
+
+        System.out.println(ch);
+    }
+}
